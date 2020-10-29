1f85bbe9d672848d88014faa61109832d1ff42f5
BugSlayer <42559788+larodriguez3@users.noreply.github.com>
2020-10-29 14:40:39 -0500

    Finished lesson 16

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,9 +6,20 @@ using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip finishSound;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem finishParticles;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,11 +29,25 @@ public class Rocket : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Thrust();
-        Rotate();
+        //  Can only rotate and RespondToThrustInput when alive
+        if(state==State.Alive){
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
-    private void Rotate() {
+    private void RespondToThrustInput() {
+        // GetKey applies all the time and will report the status of the named key.
+        if (Input.GetKey(KeyCode.Space)) {
+            ApplyThrust();
+        }
+        else {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void RespondToRotateInput() {
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         // freeze rotation before we take manual control of rotation
@@ -41,35 +66,57 @@ public class Rocket : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision collision){
+        if (state!= State.Alive){
+            return;
+        }
+
         switch (collision.gameObject.tag){
             case "Friendly":
                 print("Friendly");
                 break;
 
             case "Finish":
-                print("Hit Finish");
-                SceneManager.LoadScene(1);
+                StartTransitionSequence();
                 break;
             
             default:
-                print("Deadly");
-                SceneManager.LoadScene(0);
+                StartDyingSequence();
                 break;
         }
     }
 
-    private void Thrust() {
-        // GetKey applies all the time and will report the status of the named key.
-        if (Input.GetKey(KeyCode.Space)) {
-            // Adding force in the direction the ship is facing
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-            // so audio doesnt layer
-            if (!audioSource.isPlaying) {
-                audioSource.Play();
-            }
+    private void StartTransitionSequence(){
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finishSound);
+        finishParticles.Play();
+        Invoke("LoadNextScene", 1f);  
+    }
+
+    private void StartDyingSequence(){
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathSound);
+        deathParticles.Play();
+        Invoke("LoadCurrentScene", 1f); 
+    }
+
+    private void LoadNextScene(){
+        // TODO: allow for more than 2 levels
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadCurrentScene (){
+        SceneManager.LoadScene(0);
+    }
+
+    private void ApplyThrust(){
+        // Adding force in the direction the ship is facing
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        // so audio doesnt layer
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(mainEngine);
         }
-        else {
-            audioSource.Stop();
-        }
+        mainEngineParticles.Play();
     }
 }
